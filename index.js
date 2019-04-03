ff3528edfa8e1fbfcd5cf0fb0f3d255079072bd6
Michael Humphries <michaelhumphries1997@gmail.com>
2019-04-03 08:38:44 +0100

    adding html front page and css, modifying node server to serve the new files

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express')
+var path = require('path'); 
 var app = express()
+
+var PORT = 8081
 
 var arr = [1232,32421,3213,3423,323412]
 
 app.get('/', function(req, res){
-    res.send('hello world')
-    for(i = 0; i < arr.length; i++){
-        res.send(arr[i])
-    }
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+
+
+function getItems(){
+    for(i = 0; i < arr.length; i++){
+        var str = ''
+        str.concat(arr[i])
+    }
+    return str
+}
+
+app.listen(PORT)
+
+console.log('app listening on port', PORT)
