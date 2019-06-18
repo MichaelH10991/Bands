c89c520c9f525904ef469c26d1fd5480b952a493
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-18 18:26:33 +0100

    cleaning node code using process port

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-var express = require('express')
-var path = require('path'); 
+var express = require("express")
+var path = require("path")
 var app = express()
 
-var PORT = 8081
+var PORT = process.env.PORT || 8080
 
-var arr = [1232,32421,3213,3423,323412]
-
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-
-
-function getItems(){
-    for(i = 0; i < arr.length; i++){
-        var str = ''
-        str.concat(arr[i])
-    }
-    return str
-}
+app.use("/static", express.static(path.join(__dirname, "public")))
 
 app.listen(PORT)
 
-console.log('app listening on port', PORT)
+console.log("app listening on port", PORT)
