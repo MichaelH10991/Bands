2d3fe8c96fe608ea064c8e983734d1c35ffda1c7
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-19 18:59:33 +0100

    writing in call to database in api

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-var express = require("express")
-var path = require("path")
-var app = express()
+let express = require("express")
+let path = require("path")
+let app = express()
+let config = require("./database_config/config")
+let MongoClient = require("mongodb").MongoClient
 
-var PORT = process.env.PORT || 8080
+// database config
+let url = config.database_url
+let database_name = config.database_name
+let table = config.collection_name
+
+let PORT = process.env.PORT || 8081
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.use("/static", express.static(path.join(__dirname, "public")))
+app.route("/bands").get((req, res) => {
+  get_all_data(req, res)
+})
+
+app.use("/public", express.static(path.join(__dirname, "public")))
+
+function get_all_data(req, res) {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+    let dbo = db.db(database_name)
+    let bands = dbo.collection(table)
+    let cursor = bands.find({})
+    let arr = []
+    cursor.forEach(
+      item => {
+        if (item != null) {
+          arr.push(item)
+        }
+      },
+      err => {
+        if (err) {
+          console.log(`There was an error: ${err}`)
+        }
+        res.send(arr)
+        db.close()
+      }
+    )
+  })
+}
 
 app.listen(PORT)
 
