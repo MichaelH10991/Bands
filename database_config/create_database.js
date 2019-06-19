505930b57c8eb14a391efce60ec851368fdbe58d
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-19 19:01:43 +0100

    testing create database script

---
+++ database_config/create_database.js
@@ -0,0 +1,28 @@
+let config = require("./config")
+let MongoClient = require("mongodb").MongoClient
+
+let root_url = config.database_url
+let database = config.database_name
+let col = config.collection_name
+// must first create a MongoClient object and specify a connection URL with the correct ip address of the database.
+// mongoDB will create a database of it does not exist and creata conneciton to it
+let url = `${root_url}${database}`
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log(`there has been an error: ${err}`)
+    throw err
+  } else {
+    console.log(`database: ${database}, created at url ${url}`)
+    db.close()
+  }
+})
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  if (err) throw err
+  let dbo = db.db(database)
+  dbo.createCollection(col, (err, res) => {
+    if (err) throw err
+    console.log(`collection: ${col}, created!`)
+    db.close()
+  })
+})
