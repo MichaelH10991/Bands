92068a2a248b0ad01b2f5e837f32406e4bcd9ba5
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-24 19:06:48 +0100

    exporting get_all_events as a module

--- database_config/get_all_events.js
+++ database_config/get_all_events.js
@@ -1,12 +1,12 @@
+let config = require("./config")
+let MongoClient = require("mongodb").MongoClient
+
+// database config
+let url = config.database_url
+let database_name = config.database_name
+let table = config.collection_name
+
 module.exports = function(req, res) {
-  let config = require("./config")
-  let MongoClient = require("mongodb").MongoClient
-
-  // database config
-  let url = config.database_url
-  let database_name = config.database_name
-  let table = config.collection_name
-
   MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
     if (db) {
       let dbo = db.db(database_name)
@@ -16,7 +16,6 @@ module.exports = function(req, res) {
         .sort({ datefield: -1 })
         .toArray((err, result) => {
           if (err) throw err
-          console.log(result)
           res.send(result)
           db.close()
         })
