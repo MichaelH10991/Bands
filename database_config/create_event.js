529f9cca60c3e2c895f443f6ef8b4cd0468b9b8f
Michael Humphries <michaelhumphries1997@gmail.com>
2019-06-26 21:28:04 +0100

    adding function to create event and put into database

--- database_config/create_event.js
+++ database_config/create_event.js
@@ -7,11 +7,20 @@ let col = config.collection_name
 
 let url = root_url + database
 
-module.exports = function(req, res) {
+module.exports = function(data) {
+  const event = {
+    name: data.name,
+    support: data.support,
+    city: data.city,
+    venue: data.venue,
+    datefield: data.date,
+    day: data.day,
+    notes: data.notes
+  }
   MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
     if (err) throw err
     let dbo = db.db(database)
-    dbo.collection(col).insert(data, (err, res) => {
+    dbo.collection(col).insertOne(event, (err, res) => {
       if (err) throw err
       console.log("Number of documents inserted: " + res.insertedCount)
       db.close()
