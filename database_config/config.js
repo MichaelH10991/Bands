cc39b0876306784be0fc432c0922312e7e4564c8
Michael Humphries <michaelhumphries1997@gmail.com>
2019-07-01 20:48:46 +0100

    adding an alternative method to posting to database

--- database_config/config.js
+++ database_config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database_url: "mongodb://localhost:27017/bandsDB",
+  database_url: "mongodb://localhost:27017/",
   database_name: "bandsDB",
   collection_name: "bands"
 }
--- public/js/post_data.js
+++ public/js/post_data.js
@@ -1,11 +1,33 @@
-$("#event_form").submit(e => {
-  e.preventDefault()
-  $.post({
-    url: "/api/create",
-    type: "post",
-    data: $("#event_form").serialize(),
-    success: () => {
-      location.reload()
-    }
-  })
-})
+// $("#event_form").submit(e => {
+//   e.preventDefault()
+//   $.post({
+//     url: "/api/create",
+//     type: "post",
+//     data: $("#event_form").serialize(),
+//     success: () => {
+//       console.log("POST success")
+//       location.reload(true)
+//     }
+//   })
+// })
+
+postData("http://localhost/api/create", { answer: 42 })
+  .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+  .catch(error => console.error(error))
+
+function postData(url = "", data = {}) {
+  // Default options are marked with *
+  return fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  }).then(response => response.json()) // parses JSON response into native JavaScript objects
+}
