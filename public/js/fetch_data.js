fd947d3cf1e1b1de56d726643c787531bd2f8dec
Michael Humphries <michaelhumphries1997@gmail.com>
2019-07-11 20:05:49 +0100

    fixing this atrocious create table

--- public/js/fetch_data.js
+++ public/js/fetch_data.js
@@ -1,8 +1,7 @@
 async function get_data() {
   let res = await fetch("http://localhost:8082/api/events")
   let data = await res.json()
-  console.log("response data: ", data)
-  create_table(data)
+  await create_table(data)
 }
 get_data().catch(err => {
   console.log(`There was a fetch error: ${err}`)
@@ -12,8 +11,8 @@ function create_table(data) {
   // create table header
   let cols = ["Name", "Support", "City", "Venue", "Date", "Day", "Notes"]
   let keys = []
-  for (let i = 1; i < data.length; i++) {
-    for (let key in data[i]) {
+  for (var i = 0; i < data.length; i++) {
+    for (var key in data[i]) {
       if (keys.indexOf(key) === -1) {
         keys.push(key)
       }
@@ -37,7 +36,7 @@ function create_table(data) {
   for (let i = 0; i < data.length; i++) {
     tr = table.insertRow(-1)
 
-    for (let j = 1; j < keys.length; j++) {
+    for (let j = 1; j < keys.length - 1; j++) {
       let tabCell = tr.insertCell(-1)
       tabCell.innerHTML = data[i][keys[j]]
     }
