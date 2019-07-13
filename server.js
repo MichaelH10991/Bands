30d6d8b7294bc2cfe83158c199376ac30f61a90d
Michael Humphries <michaelhumphries1997@gmail.com>
2019-07-13 11:29:09 +0100

    adding an endpoint for test data

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let environment = require("dotenv").config()
 let path = require("path")
 let app = express()
 let bodyParser = require("body-parser")
+let test_data = require("./routes/api/test_data")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -18,5 +19,9 @@ app.use("/public", express.static(path.join(__dirname, "public")))
 
 // events API route middleware
 app.use("/api/events", require("./routes/api/events"))
+
+app.get("/api/test_data", (req, res) => {
+  res.send(test_data)
+})
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`))
