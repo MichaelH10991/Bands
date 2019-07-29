68ff8ca3e309ae370f8d3e5e84981f293bc4948c
Michael Humphries <michaelhumphries1997@gmail.com>
2019-07-29 19:47:53 +0100

    cleanup

--- routes/api/events.js
+++ routes/api/events.js
@@ -3,13 +3,6 @@ const mongoose = require("mongoose")
 const router = express.Router()
 const controller = require("./events.controller")
 
-mongoose.Promise = global.Promise
-mongoose.connect(`mongodb://localhost/bandsDB`, {
-  useNewUrlParser: true
-})
-let db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error: "))
-
 /**
  * TODO
  * - error code handling (done)
