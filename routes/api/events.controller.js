1c82a797cc43b69051df6619047c33106a308eff
Michael Humphries <michaelhumphries1997@gmail.com>
2019-08-10 11:34:31 +0100

    sending appropriate response code

--- routes/api/events.controller.js
+++ routes/api/events.controller.js
@@ -61,5 +61,6 @@ exports.remove = (req, res) => {
 // handle the errors
 function handleError(res, err) {
   console.log(err)
+  if (err.name === 'ValidationError') return res.status(400).send(err)
   return res.status(500).send(err)
 }
