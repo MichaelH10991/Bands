487c9fe7c6781a159fa98629169b4a73adbd218e
Michael Humphries <michaelhumphries1997@gmail.com>
2019-09-05 20:06:41 +0100

    ping was broken

--- routes/api/events.js
+++ routes/api/events.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("./events.controller")
 
-routeer.get("/ping", controller.ping)
+router.get("/ping", controller.ping)
 // get all events
 router.get("/", controller.index)
 // get one event
