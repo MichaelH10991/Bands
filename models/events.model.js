35788281cf180808250616346a2c01ce25a75e42
Michael Humphries <michaelhumphries1997@gmail.com>
2019-09-12 20:36:11 +0100

    updating schema to accomodate a list of support artists

--- models/events.model.js
+++ models/events.model.js
@@ -1,36 +1,17 @@
 let mongoose = require("mongoose")
 let Schema = mongoose.Schema
 
-// create a schema for our database
+// create a schemas for our database
+let supportSchema = new Schema({ name: 'string' });
+
 let eventSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  support: {
-    type: String,
-    required: true
-  },
-  city: {
-    type: String,
-    required: true
-  },
-  venue: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  day: {
-    type: String,
-    required: true
-  },
-  notes: {
-    type: String,
-    required: true
-  }
+  name: { type: String, required: true },
+  support: [supportSchema],
+  city: { type: String, required: true },
+  venue: { type: String, required: true },
+  date: { type: Date, required: true },
+  day: { type: String, required: true },
+  notes: { type: String, required: true }
 })
 
 module.exports = Event = mongoose.model("Event", eventSchema)
