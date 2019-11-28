1df4c65f3286782acd543e6e7ab5b6c864d8afb7
Michael Humphries <michaelhumphries1997@gmail.com>
2019-11-28 15:26:02 +0000

    updating deploy vulns checking

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-npm audit
-npm audit fix
-git push -f
-git push heroku master -f
+
+res=$(npm audit || true)
+regexpHigh="([0-9]+ high)"
+regexpCrit="([0-9]+ critical)"
+
+echo "$res"
+
+if [[ $res =~ $regexp ]]; then 
+    echo "found high vulns. You should fix these."
+    exit 0
+elif [[ $res =~ $regexpCrit ]]; then
+    echo "found high critical vulns. You must fix these."
+    exit 0
+fi
+
+git push
+git push heroku master
